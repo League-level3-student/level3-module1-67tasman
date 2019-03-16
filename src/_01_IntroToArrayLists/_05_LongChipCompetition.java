d9876c263a34f47cd7e72d3a376edd78a9c388ce
andrew983 <league@WTS14-4.attlocal.net>
2019-03-16 15:59:08 -0700

    Long Chip Competition, IPod Shuffle

--- src/_01_IntroToArrayLists/_05_LongChipCompetition.java
+++ src/_01_IntroToArrayLists/_05_LongChipCompetition.java
@@ -18,11 +18,45 @@ public class _05_LongChipCompetition {
 		_05_LongChipCompetition lcc = new _05_LongChipCompetition();
 		lcc.initializeBeatles();
 		//Where do i do the for loop?
-	    for (int i = 0; i < theBeatles.getSize(); i++) {
-	    	
-	    }
+	  lcc.findLongestChip();
 	}
-	
+	public void findLongestChip(){
+		double longest2 = 0;
+		int i;
+		int beatleNum = 0;
+		  for (i = 0; i < theBeatles.size(); i++) {
+		    	//System.out.println(theBeatles.get(i).getChips().size());
+		    	int numOfChips = theBeatles.get(i).getChips().size();
+		    	double longest= 0;
+		    	for (int j = 0; j < numOfChips; j++) {
+		    		double length = theBeatles.get(i).getChips().get(j).getLength();
+		    		//System.out.println(theBeatles.get(i).getChips().get(j).getLength());
+		    		if(longest < length) {
+		    			longest = length;
+		    		}
+		    	}
+		    	//System.out.println(theBeatles.get(i).getName() + " has a " + longest + " inch chip.");
+		    	String name = theBeatles.get(i).getName();
+		    	/* double georgeChip = longest;
+		    	double johnChip = longest;
+		    	double paulChip = longest;
+		    	double ringoChip = longest; */
+		    	//System.out.println(longest);
+		    
+		    	if(longest>longest2) {
+		    		longest2 = longest;
+		    		//System.out.println(i);
+		    		beatleNum++;
+		    		//System.out.println(beatleNum);
+		    		
+		    	}
+		    
+		    }
+			//System.out.println(beatleNum);
+			System.out.println(theBeatles.get(beatleNum-1).getName() + " wins");
+		//  System.out.println(longest2);
+			
+	}
 	private void initializeBeatles() {
 		Beatle george = new Beatle("George");
 		Beatle john = new Beatle("John");
--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -3,14 +3,18 @@ package _01_IntroToArrayLists;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 
 //Copyright The League of Amazing Programmers, 2015
 
 public class _06_IPodShuffle{
+	JFrame frame = new JFrame();
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
+		Song demo = new Song("demo.mp3");
+			demo.play();	
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -18,10 +22,17 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
+			ArrayList<Song> songs = new ArrayList<Song>();
+			
+			
+			  
 		
 	}
 	
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
+		//JButton button = new JButton("Suprise Me!");
+		//JFrame frame = new JFrame();
+		//frame.add(button);
 	}
 }
