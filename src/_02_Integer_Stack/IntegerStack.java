e33cd22d2ad744289db70cbeb5a68ca13298da74
League Student <league@WTS14-4.attlocal.net>
2019-06-15 15:59:09 -0700

    integer stack

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -6,49 +6,63 @@ import _01_IntroToArrayLists.Song;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	private ArrayList<Integer> songs = new ArrayList<Integer>(0);
+	private int[] ints;
+
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
+	
 	public IntegerStack() {
+		ints = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
+		int[] ints2;
+		int x = ints.length+1;
+		ints2 = new int[x];
 		
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		ints2[x-1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for(int i=0 ; i<ints.length ; i++) {
+			ints2[i] = ints[i];
+		}
 		//D. set the member array equal to the new array.
-		
+		ints= ints2;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
+		int a = ints[ints.length-1];
 		
 		//B. create a new array that is one element smaller than the member array
+		int[] ints3;
+		ints3 = new int[ints.length-1];
 		
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for(int i=0 ; i<ints3.length ; i++) {
+			ints3[i] = ints[i];
+		}
 		//D. set the member array equal to the new array
-		
+		ints = ints3;
 		//E. return the variable you created in step A
-		return 0;
+		return a;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		ints = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		int q = ints.length;
+		return q;
 	}
 }
