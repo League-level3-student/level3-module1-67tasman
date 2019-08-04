debaa83a06c2a39c3358aaf92a9cb432b47a9c89
League Student <league@iMac-14.attlocal.net>
2019-08-04 12:58:49 -0700

    Update _03_TestMatchingBrackets.java
    test matching brackets

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -16,15 +16,44 @@ public class _03_TestMatchingBrackets {
 		assertTrue(doBracketsMatch("{}{}{{}}"));
 		assertFalse(doBracketsMatch("{{}"));
 		assertFalse(doBracketsMatch("}{"));
+		_03_TestMatchingBrackets test = new _03_TestMatchingBrackets(); 
+		//test.doBracketsMatch(b);
 	}
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
 		Stack<String> a = new Stack<String>();
-		a.push("{}}");
-		System.out.println(a.pop());
+	
+		int l = a.toString().length();
+		if(l%2==1) {
+			return false;
+		}
+		int q = 0;
+		for(int i = 0; i<b.length() ; i++) {
+			
+			if(b.charAt(i)=='}'){
+				if(a.size()>0) {
+					a.pop();
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				a.push(".");
+			}
+			
+		}
+		if(a.size()>0) {
+			return false;
+		}	
+		else {
+			return true;
+			
+		}
+	
 		
-		return false;
+	
 	}
 
 }
